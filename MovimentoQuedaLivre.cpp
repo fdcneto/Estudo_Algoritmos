dcaa5dcbb2a61e86b11e50a51690a4e16ca49ad2
Francisco Neto <39269459+fdcneto@users.noreply.github.com>
2021-05-13 03:38:19 -0300

    Arquivo inicial Calcular Movimento de Queda Livre
    Algoritmo básico para cálculo da velocidade de um objeto/corpo em queda livre.
    Dados para cálculo, a saber, segundo a fórmula V = g*t
    V -> VELOCIDADE (?)
    t -> TEMPO (Informado pelo usuário)
    g -> ACELERAÇÃO da gravidade (Informado pelo usuário)

---
+++ MovimentoQuedaLivre.cpp
@@ -0,0 +1,48 @@
+//fdcneto@gmail.com
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <iomanip> //função limitar casas decimais. Ex: cout <<fixed << setprecision (2) << VALOR A SER EXIBIDO
+#include <locale> //Biblioteca que permite o uso de acentuação.
+
+#define HORA_CONVERT 3.6;
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+
+	/*A aceleração da gravidade corresponde a 9,8 m/s², isto quer dizer que um corpo em
+queda livre aumenta sua velocidade em 9,8 m/s a cada 1 segundo. */
+
+
+    //equações --> V = g * t
+    // d = (g*t²)/2
+
+    /*Onde:
+
+    V = velocidade
+    t =  tempo de queda
+    g = aceleração da gravidade
+    d = distância percorrida pelo corpo em queda
+    */
+
+    float tempoQueda, g_Aceleracao, velocidadeQueda, kmConvert;
+    cout << "CALCULAR VELOCIDADE DE QUEDA LIVRE:" << endl;
+    cout << "Informe o tempo de queda em SEGUNDOS: ";
+    cin >> tempoQueda;
+    cout << "Informe a aceleração da GRAVIDADE em m/s: "; //Obs: formato exemplo--> 9.8
+    cin >> g_Aceleracao;
+
+    cout << "\nFÓRMULA CÁLCULO: \n";
+    cout << "V = g * t \n";
+    cout << "V = " << g_Aceleracao << " m/s² * " << tempoQueda << " segundos \n";
+    velocidadeQueda = g_Aceleracao * tempoQueda;
+    kmConvert = velocidadeQueda * HORA_CONVERT;
+    cout << "Velocidade da queda = " << velocidadeQueda << " m/s ou " << kmConvert << " Km/h." << endl;
+
+
+
+	cout << "\n\n";
+	system("pause");
+	return 0;
+}
